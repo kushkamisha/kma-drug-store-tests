834e11cb6184edf246b9d9707f6cc9ba11b73668
Misha Kushka <kushka.misha@gmail.com>
2021-03-27 19:01:08 +0200

    Add /active-coponents

--- test/activeComponents.js
+++ test/activeComponents.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 const { expect } = chai;
 const apiUrl = 'https://kmadrugstore.herokuapp.com/api/v1';
 const api = axios.create({ baseURL: apiUrl });
-const url = '/categories';
+const url = '/active-components';
 
-describe(url, () => {
-  it('should get a list of categories', () => api.get(url)
+describe.only(url, () => {
+  it('should get a list of active components', () => api.get(url)
     .then(({ data }) => expect(Array.isArray(data)).to.equal(true)));
 
   it('result should be of a correct format', () => api.get(url)
-    .then(({ data }) => expect(data[0]).to.have.keys('id', 'title')));
+    .then(({ data }) => expect(data[0]).to.have.keys('id', 'name')));
 });
