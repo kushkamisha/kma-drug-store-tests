a231c08925f06490b1f21ad4746d063712bc0ce2
Misha Kushka <kushka.misha@gmail.com>
2021-03-27 17:53:14 +0200

    Basic tests setup

---
+++ test/goodsShort.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      expect(1 + 1).to.equal(2);
+    });
+  });
+});
