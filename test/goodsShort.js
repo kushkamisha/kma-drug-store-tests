076d0d4f248e43bc428cec65af6ac47f1749805a
Misha Kushka <kushka.misha@gmail.com>
2021-03-27 18:42:49 +0200

    Add /goods-short/{id}

--- test/goodsShort.js
+++ test/goodsShort.js
@@ -1,9 +1,18 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const axios = require('axios');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      expect(1 + 1).to.equal(2);
-    });
-  });
+const { expect } = chai;
+const apiUrl = 'https://kmadrugstore.herokuapp.com/api/v1';
+const api = axios.create({ baseURL: apiUrl });
+
+describe('/goods-short/{id}', () => {
+  it('should get a good', () => api.get('/goods-short/1')
+    .then(({ data }) => {
+      expect(data).to.have.keys('id', 'name', 'price', 'numAvailable', 'numInPack', 'dose', 'photo');
+    }));
+
+  it('should get a good', () => axios.get('/goods-short/9999999')
+    .catch((err) => {
+      expect(err.code).to.equal('ECONNREFUSED');
+    }));
 });
